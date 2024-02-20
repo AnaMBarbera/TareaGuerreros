74f38264cdca58fae8404a34d840d11bfba5c49b
Ana B <anabarbera90@gmail.com>
2024-02-20 17:44:33 +0100

    bottombar con botón de retroceso y home. Lógica del botón retroceso

--- app/src/main/java/es/anabarbera/fragmentosnavigation/Fragment2.kt
+++ app/src/main/java/es/anabarbera/fragmentosnavigation/Fragment2.kt
@@ -6,10 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 class Fragment2 : Fragment() {
@@ -33,9 +30,6 @@ class Fragment2 : Fragment() {
                 guerrero.setImageResource(R.drawable.conan)
             }
         }
-
-
         return view
-        //return inflater.inflate(R.layout.fragment_2, container, false)
     }
 }
--- app/src/main/java/es/anabarbera/fragmentosnavigation/MainActivity.kt
+++ app/src/main/java/es/anabarbera/fragmentosnavigation/MainActivity.kt
@@ -2,10 +2,38 @@ package es.anabarbera.fragmentosnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+
+private lateinit var navController: NavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+     // navController = Navigation.findNavController(this, R.id.idContenedor)
+       val bottomBar=findViewById<BottomNavigationView>(R.id.idBottonNav)
+
+          bottomBar.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.atras -> {
+                    // Lógica para retroceder a la vista anterior
+                    onBackPressed()
+                    true
+                }
+
+                /*R.id.home -> {
+                    // Lógica para ir al inicio
+
+                }*/
+
+                else -> false
+            }
+        }
     }
 }
